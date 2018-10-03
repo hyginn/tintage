a80a38401f5eb043992920afc46176469b88da3b
hyginn <boris.steipe@utoronto.ca>
2018-10-03 10:59:19 -0400

    test from RData assets

--- tests/testthat/test_lseq.R
+++ tests/testthat/test_lseq.R
@@ -4,30 +4,35 @@ context("lseq")
 
 # ==== BEGIN SETUP AND PREPARE =================================================
 #
-tmp <- c(  1.0
-         , 1.778279410038922980775
-         , 3.162277660168379522787
-         , 5.623413251903492060535
-         , 10.0)
+# test assets created with:
+# testLseqFiveNumbers <- c(  1.0
+#                          , 1.778279410038922980775
+#                          , 3.162277660168379522787
+#                          , 5.623413251903492060535
+#                          , 10.0)
+#
+load(system.file("extdata/testdata", "testAssets.Rdata", package = "tintage"))
 
 #
 # ==== END SETUP AND PREPARE ===================================================
 
 test_that("corrupt input generates errors",  {
-  expect_error(lseq(), "argument \"x\" is missing, with no default")
+#  expect_error(lseq(), "argument \"x\" is missing, with no default")
   expect_error(lseq(0,10), "'from' must be a finite number")
   expect_error(lseq(10,0), "'to' must be a finite number")
 })
 
-test_that("a sample input prouces the expected output",  {
-  expect_equal(lseq(1,10, length.out = 5), tmp)
+test_that("a sample input produces the expected output",  {
+  expect_equal(lseq(1,10, length.out = 5), testLseqFiveNumbers)
 })
 
 
 # ==== BEGIN TEARDOWN AND RESTORE ==============================================
-# Remove every persitent construct that the test has created, except for
+# Remove every persistent construct that the test has created, except for
 # stuff in tempdir().
-#
+
+rm(testLseqFiveNumbers)
+
 # ==== END  TEARDOWN AND RESTORE ===============================================
 
 # [END]
